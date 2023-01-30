f69d41ee84ffab9a5075bd757d74ff6825ceda5e
bill-buchan <bill.buchan@digital.justice.gov.uk>
2023-01-30 16:28:00 +0000

    Allow Setting of AWR Config

--- set_oracle_parameters/files/set_parameter_values.sh
+++ set_oracle_parameters/files/set_parameter_values.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-export ORACLE_SID=$1
-export PARAMETER_NAME=$2
-export PARAMETER_VALUE=$3
+AWR_RETENTION_DAYS=$1
 
-# Check Oracle SID exists
-/usr/local/bin/dbhome ${ORACLE_SID}
-if [[ $? -gt 0 ]]
-then
-echo "Invalid Oracle SID"
-exit 123
-fi
-
-echo "Setting $PARAMETER_NAME to $PARAMETER_VALUE"
-
-export PATH=$PATH:/usr/local/bin; 
-export ORAENV_ASK=NO ; 
-. oraenv >/dev/null;
+. ~/.bash_profile
 
 sqlplus -s /  as sysdba <<EOF
 SET LINES 1000
@@ -24,6 +10,14 @@ SET PAGES 0
 SET FEEDBACK ON
 SET HEADING OFF
 WHENEVER SQLERROR EXIT FAILURE
-ALTER SYSTEM SET $PARAMETER_NAME = $PARAMETER_VALUE ;
+
+DECLARE
+   l_retention_minutes INTEGER;
+BEGIN
+   l_retention_minutes := ${AWR_RETENTION_DAYS} * 60 * 24;
+   DBMS_WORKLOAD_REPOSITORY.modify_snapshot_settings (retention => l_retention_minutes);
+END;
+/
+
 EXIT
 EOF
